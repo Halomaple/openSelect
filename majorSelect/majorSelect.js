91d0f5595bbc594ae822cd1b42898552d3a1ca21
Jianfeng <jeff.doyle@foxmail.com>
2017-02-19 16:41:48 +0800

    Some clean up

--- majorSelect/majorSelect.js
+++ majorSelect/majorSelect.js
@@ -1,112 +1,112 @@
-$(function(){
-    var event = {
-        makeCenter : function(){
-            $('#choose-box-wrapper').css("display","block");
-            $('#choose-box-wrapper').css("position","absolute");
-            $('#choose-box-wrapper').css("top", Math.max(0, (($(window).height() - $('#choose-box-wrapper').outerHeight()) / 2) + $(window).scrollTop()) + "px");
-            $('#choose-box-wrapper').css("left", Math.max(0, (($(window).width() - $('#choose-box-wrapper').outerWidth()) / 2) + $(window).scrollLeft()) + "px");
-        },
-        initMajorClass : function(){
-            //原先的专业类别列表清空
-            $('#choose-a-majorClass').html('');
-            for(var i=0;i<majorList.length;i++)
-            {
-                $('#choose-a-majorClass').append('<a class="majorClass-item" majorClass-id="'+majorList[i].id+'">'+majorList[i].className+'</a>');
-            }
-            
-            //添加专业类别列表项的click事件
-            $('.majorClass-item').bind('click', function(){
-                    var item=$(this);
-                    var majorClass = item.attr('majorClass-id');
-                    var choosenItem = item.parent().find('.choosen');
-                    if(choosenItem){
-                        $(choosenItem).removeClass('choosen');
-                    }
-                        
-                    item.addClass('choosen');
-                    //更新专业列表
-                    event.initMajor(majorClass);
-                }
-            );
-        },
-        initMajor : function(majorClassID){
-            //原先的专业列表清空
-            $('#choose-a-major').html('');
-            var majors = majorList[majorClassID-1].class;
-            for(var i=0;i<majors.length;i++)
-            {
-                $('#choose-a-major').append('<a class="major-item" major-id="'+majors[i].id+'">'+majors[i].name+'</a>');
-            }
-            //添加专业列表项的click事件
-            $('.major-item').bind('click', function(){
-                    var item=$(this);
-                    var major = item.attr('major-id');
-                    //更新选择专业文本框中的值
-                    $('#selectMajor').val(item.text());
-                    //关闭弹窗
-                    $('#choose-box-wrapper').css("display","none");
-                }
-            );
+$(function() {
+	var event = {
+		makeCenter: function() {
+			$('#choose-box-wrapper-major').css("position", "absolute");
+			$('#choose-box-wrapper-major').css("top", Math.max(0, (($(window).height() - $('#choose-box-wrapper-major').outerHeight()) / 2) + $(window).scrollTop()) + "px");
+			$('#choose-box-wrapper-major').css("left", Math.max(0, (($(window).width() - $('#choose-box-wrapper-major').outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+		},
+		initMajorClass: function() {
+			//原先的专业类别列表清空
+			$('#choose-a-majorClass').html('');
+			for (var i = 0; i < majorList.length; i++) {
+				$('#choose-a-majorClass').append('<a class="majorClass-item" majorClass-id="' + majorList[i].id + '">' + majorList[i].className + '</a>');
+			}
 
-        },
-        selectMajor : function(){
-            $('#selectMajor').click(function(){
-                //将窗口居中
-                event.makeCenter();
+			//添加专业类别列表项的click事件
+			$('.majorClass-item').bind('click', function() {
+				var item = $(this);
+				var majorClass = item.attr('majorClass-id');
+				var choosenItem = item.parent().find('.choosen');
+				if (choosenItem) {
+					$(choosenItem).removeClass('choosen');
+				}
 
-                //初始化省份列表
-                event.initMajorClass();
+				item.addClass('choosen');
+				//更新专业列表
+				event.initMajor(majorClass);
+			});
+		},
+		initMajor: function(majorClassID) {
+			//原先的专业列表清空
+			$('#choose-a-major').html('');
+			var majors = majorList[majorClassID - 1].class;
+			for (var i = 0; i < majors.length; i++) {
+				$('#choose-a-major').append('<a class="major-item" major-id="' + majors[i].id + '">' + majors[i].name + '</a>');
+			}
+			//添加专业列表项的click事件
+			$('.major-item').bind('click', function() {
+				var item = $(this);
+				var major = item.attr('major-id');
+				//更新选择专业文本框中的值
+				$('#selectMajor').val(item.text());
+				//关闭弹窗
+				$('#choose-box-wrapper-major').hide();
+			});
 
-                //默认点击第一个专业类别
-                $($('.majorClass-item')[0]).trigger('click');
+		},
+		selectMajor: function() {
+			$('#selectMajor').click(function() {
 
-            });
-        },
-        close : function(){
-            $('#close').focus(function(){
-                $('#choose-box-wrapper').css("display","none");
-            });
-        },
-        searchMajor : function(){
-            $('#searchMajor').focus(function(){
-                $(this).val('');
-            });
-            $('#searchMajor').keyup(function(){
-                var searchVal = $(this).val();
-                var searchResult = [];
-                $.each(majorList, function(i, n){
-                        $.each(n['class'], function(k, item){
-                        if (item.name.indexOf(searchVal) != -1){
-                            searchResult.push(item);
-                        }
-                    });
-                });
+				$('#choose-box-wrapper-major').show();
 
-                $('#choose-a-major').html('');
-                for (var i=0; i < searchResult.length; i++){
-                    $('#choose-a-major').append('<a class="major-item" major-id="'+searchResult[i].id+'">'+searchResult[i].name+'</a>');
-                }
-                //添加专业列表项的click事件
-                $('.major-item').bind('click', function(){
-                        var item=$(this);
-                        var major = item.attr('major-id');
-                        //更新选择大学文本框中的值
-                        $('#selectMajor').val(item.text());
-                        //关闭弹窗
-                        $('#choose-box-wrapper').css("display","none");
-                    }
-                );
-            });
+				//将窗口居中
+				event.makeCenter();
 
-        },
-        init : function(){
-            //初始化选择、关闭
-            this.selectMajor();
-            this.close();
-            this.searchMajor();
-        }
-    };
+				//初始化省份列表
+				event.initMajorClass();
 
-    event.init();
+				//默认点击第一个专业类别
+				$($('.majorClass-item')[0]).trigger('click');
 
+			});
+		},
+		close: function() {
+			$('.choose-box-bottom .close').focus(function() {
+				$('#choose-box-wrapper-major').hide();
+			});
+		},
+		searchMajor: function() {
+			$('#searchMajor').focus(function() {
+				$(this).val('');
+			});
+			$('#searchMajor').keyup(function() {
+				var searchVal = $(this).val();
+				var searchResult = [];
+				$.each(majorList, function(i, n) {
+					$.each(n['class'], function(k, item) {
+						if (item.name.indexOf(searchVal) != -1) {
+							searchResult.push(item);
+						}
+					});
+				});
+
+				$('#choose-a-major').html('');
+				for (var i = 0; i < searchResult.length; i++) {
+					$('#choose-a-major').append('<a class="major-item" major-id="' + searchResult[i].id + '">' + searchResult[i].name + '</a>');
+				}
+				//添加专业列表项的click事件
+				$('.major-item').bind('click', function() {
+					var item = $(this);
+					var major = item.attr('major-id');
+					//更新选择大学文本框中的值
+					$('#selectMajor').val(item.text());
+					//关闭弹窗
+					$('#choose-box-wrapper-major').hide();
+				});
+			});
+
+		},
+		init: function() {
+			//初始化选择、关闭
+			this.selectMajor();
+			this.close();
+			this.searchMajor();
+		}
+	};
+
+	event.init();
+
+	$(window).resize(function() {
+		event.makeCenter();
+	});
 });
